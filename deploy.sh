afdff2aedb35a763122753f8f768bada8909e767
Alex <roopus172@gmail.com>
2026-03-07 10:46:03 +0530

    added auto change from blue->green and green->blue

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-echo "checking green container health..."
 
-STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
+ACTIVE=$(docker exec nginx grep "#server blue" /etc/nginx/nginx.conf)
 
-if [ "$STATUS" != "healthy" ]; then
-	echo "Green container is not healthy. Aborting deployment."
-	exit 1
+if [ -n "$ACTIVE" ]; then
+	echo "Green is active. Switching to blue."
+	sed -i 's/#server blue:5000;/server blue:5000;/g' nginx/nginx.conf
+	sed -i 's/server green:5000;/#server green:5000;/g' nginx/nginx.conf
+else
+	echo "Blue is active. Switching to green."
+	sed -i 's/#server green:5000;/server green:5000;/g' nginx/nginx.conf
+	sed -i 's/server blue:5000;/#server blue:5000;/g' nginx/nginx.conf
 fi
-
-echo "Green is healthy. Switching traffic..."
-
-sed -i 's/server blue:5000;/#server blue:5000;/g' nginx/nginx.conf
-sed -i 's/#server green:5000;/server green:5000;/g' nginx/nginx.conf
 
 docker compose restart nginx
 
-echo "Traffic switched to GREEN deployment"
