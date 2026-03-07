1f884c6685a75ed33543979583eec3ba03df6cb5
Alex <roopus172@gmail.com>
2026-03-07 11:15:42 +0530

    updated script to alternate using symlink

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 
-ACTIVE=$(docker exec nginx grep "#server blue" /etc/nginx/nginx.conf)
+ACTIVE=$(docker exec nginx grep "server green" /etc/nginx/upstream.conf)
 
 if [ -n "$ACTIVE" ]; then
 	echo "Green is active. Switching to blue."
-	sed -i 's/#server blue:5000;/server blue:5000;/g' nginx/nginx.conf
-	sed -i 's/server green:5000;/#server green:5000;/g' nginx/nginx.conf
+	cd nginx/
+	ln -sf upstream_blue.conf upstream.conf
+
+	#sed -i 's/#server blue:5000;/server blue:5000;/g' nginx/nginx.conf
+	#sed -i 's/server green:5000;/#server green:5000;/g' nginx/nginx.conf
 else
 	echo "Blue is active. Switching to green."
-	sed -i 's/#server green:5000;/server green:5000;/g' nginx/nginx.conf
-	sed -i 's/server blue:5000;/#server blue:5000;/g' nginx/nginx.conf
+	cd nginx/
+	ln -sf upstream_green.conf upstream.conf
+	#sed -i 's/#server green:5000;/server green:5000;/g' nginx/nginx.conf
+	#sed -i 's/server blue:5000;/#server blue:5000;/g' nginx/nginx.conf
 fi
 
 docker compose restart nginx
