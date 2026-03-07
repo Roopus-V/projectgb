ea1e6186069fd92a9a34e25c4de49b24e5f93887
Alex <roopus172@gmail.com>
2026-03-07 11:46:20 +0530

    removed cd and made required changes

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,16 @@ ACTIVE=$(docker exec nginx grep "server green" /etc/nginx/upstream.conf)
 
 if [ -n "$ACTIVE" ]; then
 	echo "Green is active. Switching to blue."
-	cd nginx/
-	ln -sf upstream_blue.conf upstream.conf
+	ln -sf upstream_blue.conf nginx/upstream.conf
 
 	#sed -i 's/#server blue:5000;/server blue:5000;/g' nginx/nginx.conf
 	#sed -i 's/server green:5000;/#server green:5000;/g' nginx/nginx.conf
 else
 	echo "Blue is active. Switching to green."
-	cd nginx/
-	ln -sf upstream_green.conf upstream.conf
+	ln -sf upstream_green.conf nginx/upstream.conf
 	#sed -i 's/#server green:5000;/server green:5000;/g' nginx/nginx.conf
 	#sed -i 's/server blue:5000;/#server blue:5000;/g' nginx/nginx.conf
 fi
 
-docker compose restart nginx
+docker exec nginx nginx -s reload
 
