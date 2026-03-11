d70fc8668896a6d239b461325f44f6861d1393f9
Alex <roopus172@gmail.com>
2026-03-11 16:37:58 +0530

    added rolling canary deployment

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,88 @@
 #!/bin/bash
 
+echo "checking green container health..."
+STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
 
-ACTIVE=$(docker exec nginx grep "server green" /etc/nginx/upstream.conf)
-
-if [ -n "$ACTIVE" ]; then
-	echo "Green is active. Switching to blue."
-	ln -sf upstream_blue.conf nginx/upstream.conf
-
-	#sed -i 's/#server blue:5000;/server blue:5000;/g' nginx/nginx.conf
-	#sed -i 's/server green:5000;/#server green:5000;/g' nginx/nginx.conf
-else
-	echo "Blue is active. Switching to green."
-	ln -sf upstream_green.conf nginx/upstream.conf
-	#sed -i 's/#server green:5000;/server green:5000;/g' nginx/nginx.conf
-	#sed -i 's/server blue:5000;/#server blue:5000;/g' nginx/nginx.conf
+if [ "$STATUS" != "healthy" ]; then
+	echo "Green container is not healthy. Aborting deployment."
+	exit 1
 fi
 
+echo "Green is healthy. Starting canary phase..."
+
+
+#at 10%
+ln -sf upstream_1.conf nginx/upstream.conf
 docker exec nginx nginx -s reload
+sleep 10
+STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
+
+if [ "$STATUS" != "healthy" ]; then
+	echo "Green became unhealthy. Rolling back to BLUE."
+
+	ln -sf upstream_0.conf nginx/upstream.conf
+	docker exec nginx nginx -s reload
+
+	exit 1
+fi
+echo "Green is healthy at 10%. Increasing to 30%..."
+
+
+#At 30%
+ln -sf upstream_3.conf nginx/upstream.conf
+docker exec nginx nginx -s reload
+sleep 10
+STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
+
+if [ "$STATUS" != "healthy" ]; then
+	echo "Green became unhealthy. Rolling back to BLUE."
+
+	ln -sf upstream_0.conf nginx/upstream.conf
+	docker exec nginx nginx -s reload
+
+	exit 1
+fi
+echo "Green is healthy at 30%. Increasing to 50%..."
+
+
+#at 50%
+ln -sf upstream_5.conf nginx/upstream.conf
+docker exec nginx nginx -s reload
+sleep 10
+STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
+
+if [ "$STATUS" != "healthy" ]; then
+	echo "Green became unhealthy. Rolling back to BLUE."
+
+	ln -sf upstream_0.conf nginx/upstream.conf
+	docker exec nginx nginx -s reload
+
+	exit 1
+fi
+echo "Green is healthy at 50%. Increasing to 100%..."
+
+
+#at 100%v green
+ln -sf upstream_green.conf nginx/upstream.conf
+docker exec nginx nginx -s reload
+
+echo "Monitoring deployment..."
+
+sleep 15
+
+
+STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
+
+if [ "$STATUS" != "healthy" ]; then
+	echo "Green became unhealthy. Rolling back to BLUE."
+
+	ln -sf upstream_0.conf nginx/upstream.conf
+	docker exec nginx nginx -s reload
+
+	exit 1
+fi
+
+echo "Deployment successful. Green is stable."
+
+
 
