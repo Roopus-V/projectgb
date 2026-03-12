c82cc52e691dfdd28199aca6b9ceac186153acae
Alex <roopus172@gmail.com>
2026-03-12 16:32:35 +0530

    Updated script with functions and loop to reduce code redudancy

--- deploy.sh
+++ deploy.sh
@@ -2,89 +2,105 @@
 
 set -e
 
-echo "checking green container health..."
-STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
+NGINX_CONTAINER="nginx"
+UPSTREAM="nginx/upstream.conf"
 
-if [ "$STATUS" != "healthy" ]; then
-	echo "Green container is not healthy. Aborting deployment."
-	exit 1
+STAGES=(1 3 5)
+
+#function to generate upstream
+generate_upstream(){
+        BLUE=$1
+        GREEN=$2
+
+        if [ "$BLUE" -eq 0 ]; then
+			ln -sf upstream_green.conf $UPSTREAM
+        elif [ "$GREEN" -eq 0 ]; then
+			ln -sf upstream_blue.conf $UPSTREAM
+        elif [ "$GREEN" -eq 1 ]; then
+			ln -sf upstream_1.conf $UPSTREAM
+        elif [ "$GREEN" -eq 3 ]; then
+			ln -sf upstream_3.conf $UPSTREAM
+        elif [ "$GREEN" -eq 5 ]; then
+			ln -sf upstream_5.conf $UPSTREAM
+	fi
+}
+
+#function to reload
+reload_nginx(){
+        docker exec $NGINX_CONTAINER nginx -t
+        docker exec $NGINX_CONTAINER nginx -s reload
+}
+#function to check health of container
+
+check_health(){
+        STATUS=$(docker inspect --format='{{.State.Health.Status}}' $TARGET)
+        if [ "$STATUS" != "healthy" ]; then
+                rollback
+        fi
+}
+
+#function to rollback if there are issues.
+rollback(){
+        echo "Rollback triggered. Restoring to previous environment."
+
+        if [ "$TARGET" == "green" ]; then
+                generate_upstream 10 0
+        else
+                generate_upstream 0 10
+        fi
+        reload_nginx
+        exit 1
+}
+
+echo "Dectecting environment...."
+
+ACTIVE=$(readlink $UPSTREAM || true )
+
+if [[ "$ACTIVE" == *green* ]]; then
+        TARGET="blue"
+        echo "Green is Live. Deploying to Blue..."
+else
+        TARGET="green"
+        echo "Blue is Live. Deploying to Green..."
 fi
 
-echo "Green is healthy. Starting canary phase..."
+echo "Checking $TARGET health..."
+check_health
 
+echo "Starting progressive rollout."
 
-#at 10%
-ln -sf upstream_1.conf nginx/upstream.conf
-docker exec nginx nginx -s reload
-sleep 10
-STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
+#Loop
+for s in "${STAGES[@]}"; do
 
-if [ "$STATUS" != "healthy" ]; then
-	echo "Green became unhealthy. Rolling back to BLUE."
+        if [ "$TARGET" == "green" ]; then
+                BLUE=$((10 - s))
+                GREEN=$s
+        else
+                BLUE=$s
+                GREEN=$((10 - s))
+        fi
 
-	ln -sf upstream_0.conf nginx/upstream.conf
-	docker exec nginx nginx -s reload
+        echo "Traffic stage: blue=$BLUE; green=$GREEN"
 
-	exit 1
+        generate_upstream $BLUE $GREEN
+        reload_nginx
+        sleep 10
+        check_health
+
+done
+
+echo "Promoting $TARGET to 100%..."
+
+if [ "$TARGET" == "green" ]; then
+        generate_upstream 0 10
+else
+        generate_upstream 10 0
 fi
-echo "Green is healthy at 10%. Increasing to 30%..."
 
-
-#At 30%
-ln -sf upstream_3.conf nginx/upstream.conf
-docker exec nginx nginx -s reload
-sleep 10
-STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
-
-if [ "$STATUS" != "healthy" ]; then
-	echo "Green became unhealthy. Rolling back to BLUE."
-
-	ln -sf upstream_0.conf nginx/upstream.conf
-	docker exec nginx nginx -s reload
-
-	exit 1
-fi
-echo "Green is healthy at 30%. Increasing to 50%..."
-
-
-#at 50%
-ln -sf upstream_5.conf nginx/upstream.conf
-docker exec nginx nginx -s reload
-sleep 10
-STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
-
-if [ "$STATUS" != "healthy" ]; then
-	echo "Green became unhealthy. Rolling back to BLUE."
-
-	ln -sf upstream_0.conf nginx/upstream.conf
-	docker exec nginx nginx -s reload
-
-	exit 1
-fi
-echo "Green is healthy at 50%. Increasing to 100%..."
-
-
-#at 100%v green
-ln -sf upstream_10.conf nginx/upstream.conf
-docker exec nginx nginx -s reload
+reload_nginx
 
 echo "Monitoring deployment..."
-
 sleep 15
+check_health
 
-
-STATUS=$(docker inspect --format='{{.State.Health.Status}}' green)
-
-if [ "$STATUS" != "healthy" ]; then
-	echo "Green became unhealthy. Rolling back to BLUE."
-
-	ln -sf upstream_0.conf nginx/upstream.conf
-	docker exec nginx nginx -s reload
-
-	exit 1
-fi
-
-echo "Deployment successful. Green is stable."
-
-
-
+echo "Deployment successful."
